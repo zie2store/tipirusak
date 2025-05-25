f5a8f5071c5eaa74343aa41eff5ca515e9f9e97b
zie2store <kuenastar101@gmail.com>
2025-05-26 00:59:27 +0700

    Update stream.js

--- api/stream.js
+++ api/stream.js
@@ -5,8 +5,8 @@ export default async function handler(req, res) {
     const response = await fetch(streamUrl, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Mobile/15E148 Safari/604.1',
-        'Referer': 'https://cookiewebplay.xyz/',
-        'Origin': 'https://cookiewebplay.xyz',
+        'Referer': 'https://antenacentral.store/',
+        'Origin': 'https://antenacentral.store/',
       }
     });
 

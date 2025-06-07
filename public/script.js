2822a46196946bc86373f719c8e8d02af83a1fa5
zie2store <kuenastar101@gmail.com>
2025-06-07 23:09:39 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -54,7 +54,7 @@
             
             // Set title
             titleEl.innerHTML = `<h1>${doc.Title}</h1>`;
-
+            <hr class="post-divider">
             // Set description
             descEl.innerHTML = `
               <p class="description">
@@ -97,6 +97,7 @@
                   </a></div>
                   <div class="related-post-text">
                     ${d.Summary}
+                    <hr class="post-divider">
                   </div>
                </div>
               `;
@@ -104,6 +105,7 @@
 
             suggEl.innerHTML = `
             <h2>These documents might be related to ${doc.Title}</h2>
+            <hr class="post-divider">
               ${suggestions}
             `;
           }
